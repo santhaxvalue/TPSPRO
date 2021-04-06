d31895ed72363618aff52be2e3398bf5ac057d8d
Santhaxvalue <santhakumar.xvalue@gmail.com>
2021-04-06 23:36:35 +0530

    product url changed

--- app/src/qa/java/com.config/Config.java
+++ app/src/qa/java/com.config/Config.java
@@ -2,6 +2,10 @@ package com.config;
 
 
 public class Config {
-    public static final String BASE_URL = "http://182.74.71.249:302/api/psapi/";
+//    public static final String BASE_URL = "http://182.74.71.249:302/api/psapi/";
+      public static final String BASE_URL = "http://communisense.borderlessaccess.com:9094/api/psapi/";
+
+
+
     public static final boolean DEBUG = true;
 }
