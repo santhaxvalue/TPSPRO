360fd3ae99be99f6bb8541827e462f02d1a4d68f
Santhaxvalue <santhakumar.xvalue@gmail.com>
2021-05-03 20:56:13 +0530

    DOB issue fixed,and DOB traslation file added

--- app/src/main/java/com/panelManagement/fragment/RewardPointsFragment.java
+++ app/src/main/java/com/panelManagement/fragment/RewardPointsFragment.java
@@ -310,12 +310,25 @@ public class RewardPointsFragment extends BaseFragment implements OnClickListene
         int month = cal.get(Calendar.MONTH);
         int day = cal.get(Calendar.DAY_OF_MONTH);
 
+//        DatePickerDialog dialog = new DatePickerDialog(
+//                getActivity(),
+//                android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+//                mDateSetListener,
+//                year,month,day);
+
+//        DatePickerDialog dialog = new DatePickerDialog(
+//                getActivity(),
+//                android.R.style.Theme_Material_Dialog_MinWidth,
+//                mDateSetListener,
+//                year,month,day);
+
         DatePickerDialog dialog = new DatePickerDialog(
                 getActivity(),
-                android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                android.R.style.Theme_Material_Dialog_NoActionBar_MinWidth,
                 mDateSetListener,
                 year,month,day);
-        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+//        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         dialog.show();
         dialog.setCanceledOnTouchOutside(false);
 
